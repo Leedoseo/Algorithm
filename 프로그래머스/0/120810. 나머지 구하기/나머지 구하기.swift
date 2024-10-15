37e28a09ec3331ee3312afba0218cb0eb95c0afb
Kimkh <168413396+Leedoseo@users.noreply.github.com>
2024-10-15 16:26:11 +0530

    Update 나머지 구하기.swift

--- 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.swift
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.swift
@@ -6,3 +6,17 @@ func solution(_ num1:Int, _ num2:Int) -> Int {
     }
     return num1 % num2
 }
+
+// 다른 풀이
+// import Foundation
+
+// func solution(_ num1:Int, _ num2:Int) -> Int {
+//     return num1 % num2
+// }
+
+// import Foundation
+
+// func solution(_ num1:Int, _ num2:Int) -> Int {
+//     var num3 : Int = num1 % num2
+//     return num3
+// }
