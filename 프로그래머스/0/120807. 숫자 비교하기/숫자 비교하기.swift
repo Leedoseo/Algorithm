daf04c863e4801402667f0814864fa908a94ad47
Kimkh <168413396+Leedoseo@users.noreply.github.com>
2024-10-16 05:30:10 +0530

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    
+    guard(num1 >= 0 && num1 <= 10000) , (num2 >= 0 && num2 <= 10000) else {
+        return 0
+    }
+    
+    return num1 == num2 ? 1 : -1
+    
+}
